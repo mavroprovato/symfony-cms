44c1627a2925496927d5c6db47487b9296ac3025
Kostas Kokkoros <mavroprovato@gmail.com>
2018-02-17 03:44:00 +0200

    Display content in the home page

---
+++ assets/js/app.js
@@ -0,0 +1,12 @@
+require('jquery');
+require('jquery-slim');
+require('popper.js');
+require('bootstrap');
+Holder = require('holderjs');
+require('../css/app.scss');
+
+Holder.addTheme('thumb', {
+    bg: '#55595c',
+    fg: '#eceeef',
+    text: 'Thumbnail'
+});
---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Content;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends Controller
+{
+
+    /**
+     * The default controller.
+     *
+     * @Route("/")
+     * @return string
+     */
+    public function index()
+    {
+        $repository = $this->getDoctrine()->getRepository(Content::class);
+        $contents = $repository->findBy([], ['createdAt' => 'DESC'], 10);
+
+        return $this->render('index.html.twig', ['contents' => $contents]);
+    }
+}
