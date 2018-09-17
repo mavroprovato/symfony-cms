c4954ae80d2c096397bb805d2ea751b612088ee8
Kostas Kokkoros <mavroprovato@gmail.com>
2018-09-18 00:37:52 +0300

    Change layout

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,4 @@
 require('jquery');
-require('jquery-slim');
 require('popper.js');
 require('bootstrap');
-Holder = require('holderjs');
 require('../css/app.scss');
-
-Holder.addTheme('thumb', {
-    bg: '#55595c',
-    fg: '#eceeef',
-    text: 'Thumbnail'
-});
