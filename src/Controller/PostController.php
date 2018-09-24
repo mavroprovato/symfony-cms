654e1407a9008a184ab8fe145004c8a3a7549473
Kostas Kokkoros <mavroprovato@gmail.com>
2018-09-24 23:05:57 +0300

    Controller methods for day, month and year posts with pagination

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -27,25 +27,106 @@ class PostController extends Controller
     }
 
     /**
-     * Display the index page.
+     * Display the post list page.
      *
-     * @Route("/", name="index", methods={"GET"})
+     * @Route("/", name="posts", methods={"GET"})
      * @return string
      */
-    public function index()
+    public function list()
     {
-        return $this->page('1');
+        return $this->dayPage();
     }
 
     /**
-     * Display the index page.
+     * Display the post list page.
      *
-     * @Route("/page/{page}", name="index_page", methods={"GET"}, requirements={"page"="\d+"})
+     * @Route(
+     *     path="/page/{page}",
+     *     name="posts_page",
+     *     methods={"GET"},
+     *     requirements={"page"="\d+"},
+     *     defaults={"page"="1"}
+     * )
+     * @return string
+     */
+    public function page($page)
+    {
+        return $this->dayPage($page);
+    }
+
+    /**
+     * Display the day post list.
+     *
+     * @Route(
+     *     path="/{year}/{month}/{day}",
+     *     name="posts_date",
+     *     methods={"GET"},
+     *     requirements={"year"="\d+", "month"="\d+", "day"="\d+"},
+     *     defaults={"year"=null, "month"=null, "day"=null}
+     * )
+     * @return string
+     */
+    public function listDay($year = null, $month = null, $day = null)
+    {
+        return $this->dayPage('1', $year, $month, $day);
+    }
+
+    /**
+     * Display the year page.
+     *
+     * @Route(
+     *     "/{year}/page/{page}",
+     *     name="posts_year_page",
+     *     methods={"GET"},
+     *     requirements={"page"="\d+", "year"="\d+"},
+     *     defaults={"page"="1", "year"=null}
+     * )
      * @param string $page The page number.
      * @return \Symfony\Component\HttpFoundation\Response The response.
      */
-    public function page(string $page = '1')
+    public function yearPage(string $page = '1', $year = null)
     {
-        return $this->render('index.html.twig', $this->postService->list(intval($page)));
+        return $this->dayPage($page, $year);
+    }
+
+    /**
+     * Display the month page.
+     *
+     * @Route(
+     *     "/{year}/{month}/page/{page}",
+     *     name="posts_month_page",
+     *     methods={"GET"},
+     *     requirements={"page"="\d+", "year"="\d+", "month"="\d+"},
+     *     defaults={"page"="1", "year"=null}
+     * )
+     * @param string $page The page number.
+     * @return \Symfony\Component\HttpFoundation\Response The response.
+     */
+    public function monthPage(string $page = '1', $year = null, $month = null)
+    {
+        return $this->dayPage($page, $year, $month);
+    }
+
+    /**
+     * Display the day page.
+     *
+     * @Route(
+     *     "/{year}/{month}/{day}/page/{page}",
+     *     name="posts_day_page",
+     *     methods={"GET"},
+     *     requirements={"page"="\d+", "year"="\d+", "month"="\d+", "day"="\d+"},
+     *     defaults={"page"="1", "year"=null, "month"=null, "day"=null}
+     * )
+     * @param string $page The page number.
+     * @return \Symfony\Component\HttpFoundation\Response The response.
+     */
+    public function dayPage(string $page = '1', $year = null, $month = null, $day = null)
+    {
+        return $this->render('index.html.twig', $this->postService->list(
+            intval($page),
+            $year === null ? null : intval($year),
+            $month === null ? null : intval($month),
+            $day === null ? null : intval($day)
+        ));
     }
 }
--- src/Service/PostService.php
+++ src/Service/PostService.php
@@ -19,7 +19,7 @@ class PostService
     private $paginator;
 
     /**
-     * Create the controller.
+     * Create the post service.
      *
      * @param PostRepository $postRepository The post repository.
      * @param PaginatorInterface $paginator The paginator interface.
@@ -34,20 +34,70 @@ class PostService
      * Return a post list page.
      *
      * @param int $page The page number.
+     * @param int|null $year The year of the posts to include. If null, fetch all posts.
+     * @param int|null $month The month of the posts to include. If null, fetch all posts in the year.
+     * @param int|null $day The day of the posts to include. If null, fetch all posts in the month.
      * @return array The model for the page.
      */
-    public function list(int $page = 1)
-    {
+    public function list(int $page = 1, int $year = null, int $month = null, int $day = null)
+    {dump("page: $page, year: $year, month: $month, day: $day");
         // Fetch contents by publication date
-        $contentsQuery = $this->postRepository->createQueryBuilder('c')
-            ->orderBy('c.publishedAt', 'DESC')
-            ->getQuery();
-        $contents = $this->paginator->paginate($contentsQuery, $page, 10);
+        $queryBuilder = $this->postRepository->createQueryBuilder('p');
+        list($startDate, $endDate) = $this->getStartEnd($year, $month, $day);
+
+        if ($startDate !== null && $endDate != null) {
+            $queryBuilder->andWhere(
+                $queryBuilder->expr()->andX(
+                    $queryBuilder->expr()->gte('p.publishedAt', ':startDate'),
+                    $queryBuilder->expr()->lt('p.publishedAt', ':endDate')
+                )
+            );
+            $queryBuilder->setParameter('startDate', $startDate);
+            $queryBuilder->setParameter('endDate', $endDate);
+        }
+        $queryBuilder = $queryBuilder->orderBy('p.publishedAt', 'DESC');
+        $query = $queryBuilder->getQuery();
+        $contents = $this->paginator->paginate($query, $page, 10);
         $archives = $this->postRepository->countByMonth();
 
         return [
             'contents' => $contents,
             'archives' => $archives
         ];
+    }
+
+    /**
+     * Returs the start and dates that should be added to the query that restricts the posts to be fetched.
+     *
+     * @param int|null $year The post year. If null, fetch all posts.
+     * @param int|null $month The post month. If null, fetch all posts in the year.
+     * @param int|null $day The post day. If null, fetch all posts in the month.
+     * @return array A two element array, with the start and end @see DateTime for the restriction.
+     */
+    private function getStartEnd(int $year = null, int $month = null, int $day = null)
+    {
+        $startDate = new \DateTime();
+        $startDate->setTime(0, 0);
+        $endDate = new \DateTime();
+        $endDate->setTime(0, 0);
+
+        if ($year !== null && $month != null && $day != null) {
+            $startDate = $startDate->setDate($year, $month, $day);
+            $endDate = clone $startDate;
+            $endDate->modify('+1 day');
+        } elseif ($year !== null && $month != null) {
+            $startDate = $startDate->setDate($year, $month, 1);
+            $endDate = clone $startDate;
+            $endDate->modify('+1 month');
+        } elseif ($year !== null) {
+            $startDate = $startDate->setDate($year, 1, 1);
+            $endDate = clone $startDate;
+            $endDate->modify('+1 year');
+        } else {
+            $startDate = null;
+            $endDate = null;
+        }
+
+        return [$startDate, $endDate];
     }
 }
