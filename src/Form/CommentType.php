97b4dc188845f5183664af789e1bceae02462be9
Kostas Kokkoros <mavroprovato@gmail.com>
2018-11-26 00:47:48 +0200

    Add translations

--- src/Form/CommentType.php
+++ src/Form/CommentType.php
@@ -6,6 +6,7 @@ use App\Entity\Comment;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\UrlType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -23,7 +24,9 @@ class CommentType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
+            ->add('name', TextType::class, [
+                'label' => 'post.comment.name'
+            ])
             ->add('email', EmailType::class, [
                 'required' => false
             ])
