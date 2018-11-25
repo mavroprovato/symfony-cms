b754d9cdef5ffc65409f1aed5bc219f79d83b45a
Kostas Kokkoros <mavroprovato@gmail.com>
2018-11-25 22:33:31 +0200

    Custom login page

---
+++ assets/js/login.js
@@ -0,0 +1,2 @@
+require('bootstrap');
+require('../css/login.scss');
--- webpack.config.js
+++ webpack.config.js
@@ -8,24 +8,11 @@ Encore
     // only needed for CDN's or sub-directory deploy
     //.setManifestKeyPrefix('build/')
 
-    /*
-     * ENTRY CONFIG
-     *
-     * Add 1 entry for each "page" of your app
-     * (including one that's included on every page - e.g. "app")
-     *
-     * Each entry will result in one JavaScript file (e.g. app.js)
-     * and one CSS file (e.g. app.css) if you JavaScript imports CSS.
-     */
+    // Main application
     .addEntry('app', './assets/js/app.js')
+    .addEntry('login', './assets/js/login.js')
 
-    /*
-     * FEATURE CONFIG
-     *
-     * Enable & configure other features below. For a full
-     * list of features, see:
-     * https://symfony.com/doc/current/frontend.html#adding-more-features
-     */
+    // Feature configuration
     .cleanupOutputBeforeBuild()
     .enableBuildNotifications()
     .enableSourceMaps(!Encore.isProduction())
